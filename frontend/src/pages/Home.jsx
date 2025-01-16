faa9434b2a543f47391fbafdd3cb5f3ad7ffcaab
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-01-16 17:25:52 +0530

    trying sum worked

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Hero from '../components/Hero'
 import CountdownTimer from '../components/CountdownTimer'
-import KeynoteSpeakerCard from '../components/Keynote_car'
+import KeynoteSpeakerCard from '../components/Keynote_card'
 import hackathon from '../images/HACKATHON.png'
 import partners from '../images/PARTNERS.png'
 import workshops from '../images/WORKSHOPS.png'
