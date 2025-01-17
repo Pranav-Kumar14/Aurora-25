67ba97d6cdcc0c5a8be359024c884477d88debe4
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-01-17 16:23:40 +0530

    Update Keynote_card.jsx

--- frontend/src/components/Keynote_card.jsx
+++ frontend/src/components/Keynote_card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const KeynoteSpeakerCard = () => {
   return (
@@ -24,11 +25,11 @@ const KeynoteSpeakerCard = () => {
       </div>
       {/* Button */}
       <div className="text-center mt-6">
-        <link
+        <Link
         to='/speaker'
         className="bg-blue-600 hover:bg-blue-700 text-white px-6 sm:px-8 py-2 sm:py-3 rounded-full text-sm sm:text-base lg:text-lg font-medium shadow-md">
           Know More
-        </link>
+        </Link>
       </div>
     </div>
   );
