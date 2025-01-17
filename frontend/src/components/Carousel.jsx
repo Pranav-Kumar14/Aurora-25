12d423bfafc0e5e404a324861ffb39a70fc2a4ff
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-01-17 13:36:10 +0530

    carousel updates
    workshop carousel needs work
    partners carousel needs fixing T_T

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -31,3 +31,39 @@ const Carousel = () => {
             <button
                 className="relative mx-6 px-6 left-4 text-white bg-gray-800 p-2 rounded-full hover:bg-gray-700"
                 onClick={handlePrev}
+            >
+                &#8249;
+            </button>
+
+            {/* Carousel items */}
+            <div className="flex items-center space-x-6">
+                {getVisibleItems().map((item, index) => (
+                    <div
+                        key={item.id}
+                        className={`w-24 h-24 rounded-full flex items-center justify-center transition-all duration-300 ${
+                            index === Math.floor(visibleCount / 2)
+                                ? "scale-110 bg-white shadow-lg"
+                                : "scale-90 opacity-50"
+                        }`}>
+
+                        <img
+                            src={item.src}
+                            alt={item.alt}
+                            className="w-16 h-16 object-contain rounded-full"
+                        />
+                    </div>
+                ))}
+            </div>
+
+            {/* Right arrow */}
+            <button
+                className="relative mx-6 px-6 right-4 text-white bg-gray-800 p-2 rounded-full hover:bg-gray-700"
+                onClick={handleNext}
+            >
+                &#8250;
+            </button>
+        </div>
+    );
+};
+
+export default Carousel;
