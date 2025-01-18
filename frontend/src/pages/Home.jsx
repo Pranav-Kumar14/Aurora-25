7cffc0e3eb6c66f69f7bf3fed4f8a3ffbf2e7afa
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-01-18 18:17:49 +0530

    Home spacing update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,19 +11,30 @@ import InfiniteCarousel from '../components/slider'
 const Home = () => {
     return (
         <div className="bg-gradient-to-r from-[#0f0d39] to-[#201867]">
-            <Hero />
-            <CountdownTimer targetDate={new Date("2025-01-23T00:00:00")} />
-            <KeynoteSpeakerCard />
+            <div className='mt-6 py-6'>
+                <Hero />
+            </div>
+            <div className='py-12'>
+                <CountdownTimer targetDate={new Date("2025-01-23T00:00:00")} />
+            </div>
+            <div className='mt-8 py-12'>
+                <KeynoteSpeakerCard />
+            </div>
+            <div className="mt-6 py-12">
+                <img src={partners} alt="partners" className="w-1/4 py-8 mx-auto" />
+                <InfiniteCarousel />
+            </div>
+            <div className="py-12">
+                <img src={workshops} alt="workshops" className="w-1/4 py-8 mx-auto" />
+                <Carousel />
+            </div>
 
-            <img src={partners} alt="partners" className="w-1/4 py-8 mx-auto" />
-            <InfiniteCarousel />
-
-            <img src={workshops} alt="workshops" className="w-1/4 py-8 mx-auto" />
-            <Carousel />
-
+            <div className='py-12 mb-6'>
             <img src={hackathon} alt="hackathon" className="w-1/4 py-8 mx-auto" />
 
-            <HackathonCard />
+<HackathonCard />
+            </div>
+
 
         </div>
     )
